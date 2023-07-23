959f2d9f28d0d6fd413d227ded27ce3d6de4e0dd
Kundan241 <58340886+Kundan241@users.noreply.github.com>
2023-07-23 22:26:17 +0530

    styling-begin-with-footer

--- quiz.js
+++ quiz.js
@@ -1,15 +1,31 @@
 // Corrected JavaScript
 const submitButton = document.querySelector("#btn-summitAnswer");
 const formAnswers = document.querySelector("#quiz-form");
+const outputQuiz=document.querySelector("#outputQuiz");
 
+let index=0;
+let correctAnswerArray = ["90°", "one right angle", "14, 15, 26", "Equilateral triangle", "85°", "10°", "a + b + c", "no", "45°"];
 
 
 submitButton.addEventListener("click",function clickHandler()
 {
         let result= new FormData(formAnswers);
-        
-        for(let a of  result.values)
+        let score=0;
+        for(let [key,value] of  result)
         {
-            console.log(a);
+
+            if(correctAnswerArray[index]==value){
+                score=score+1;
+            }
+            else{
+                score=score-1;
+            }
+            console.log(score);
+            index=index
+            =1;
         }
+        console.log(score)
+        
+        outputQuiz.innerHTML="your score is "+score;
+
 });
